c4737467df71c8c3990aebb29390a9b51f96b46e
Christopher Gammie <contact@gammie.co.uk>
2022-01-08 14:41:32 +0000

    Update core dependency

--- tests/lib/Acceptance/TestCase.php
+++ tests/lib/Acceptance/TestCase.php
@@ -34,6 +34,7 @@ use LaravelJsonApi\Core\Resources\Container as ResourceContainer;
 use LaravelJsonApi\Core\Resources\Factory;
 use LaravelJsonApi\Core\Schema\Container as SchemaContainer;
 use LaravelJsonApi\Core\Store\Store;
+use LaravelJsonApi\Core\Support\ContainerResolver;
 use Orchestra\Testbench\TestCase as BaseTestCase;
 
 class TestCase extends BaseTestCase
@@ -49,28 +50,28 @@ class TestCase extends BaseTestCase
 
         $this->withoutDeprecationHandling();
 
-        $this->app->singleton(SiteStorage::class, fn() => new SiteStorage(
-            $this->app->make(UserStorage::class),
-            $this->app->make(TagStorage::class),
+        $this->app->singleton(SiteStorage::class, static fn($container) => new SiteStorage(
+            $container->make(UserStorage::class),
+            $container->make(TagStorage::class),
             require __DIR__ . '/../../storage/sites.php'
         ));
 
-        $this->app->singleton(UserStorage::class, fn() => new UserStorage(
+        $this->app->singleton(UserStorage::class, static fn() => new UserStorage(
             require __DIR__ . '/../../storage/users.php'
         ));
 
-        $this->app->singleton(TagStorage::class, fn() => new TagStorage(
+        $this->app->singleton(TagStorage::class, static fn() => new TagStorage(
             require __DIR__ . '/../../storage/tags.php'
         ));
 
-        $this->app->singleton(
-            SchemaContainerContract::class,
-            fn() => new SchemaContainer($this->app, $this->app->make(Server::class), [
+        $this->app->singleton(SchemaContainerContract::class, static function ($container) {
+            $resolver = new ContainerResolver(static fn () => $container);
+            return new SchemaContainer($resolver, $container->make(Server::class), [
                 SiteSchema::class,
                 TagSchema::class,
                 UserSchema::class,
-            ]),
-        );
+            ]);
+        });
 
         $this->app->singleton(Server::class, function () {
             $server = $this->createMock(Server::class);
